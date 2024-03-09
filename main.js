baf8af58b325401d4607213a0377e8ed0db8906a
bek123456789 <redmirating@gmail.com>
2024-03-09 22:00:45 +0500

    done

---
+++ main.js
@@ -0,0 +1,8 @@
+glet string = prompt('String typedagi malumotni kriting')
+let number = prompt('Number typedagi malumotni kriting')
+let boolean = prompt('Boolean typedagi malumotni kriting')
+alert('Hamma malumotlarni togri kiritkaningizga ishonchingiz komilmi?')
+alert(`Siz yozgan ${string} sozi string typega tegishli\nSiz yozgan ${number} sozi number typega tegishli\nSiz yozgan ${boolean} sozi boolean typega tegishli`)
+console.log('String: ' + first);
+console.log('Number: ' + second);
+console.log('Boolen: ' + third);
